147e9f71f4fe0fc5e65ac1c3b02e493c260aeb16
Tymur Faradzhev <faradzhev@users.noreply.github.com>
2018-12-15 00:28:20 +0200

    Create ooo.cpp

---
+++ ooo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#define get do
+#define out cout
+#define of <<
+#define the ' ' of
+#define office return
+#define now int
+#define man while
+
+using namespace std;
+
+now main() {
+    now o = 0;
+    o += ++o of o;
+    now oo = o;
+    oo += oo--;
+    get {
+        out of 'o';
+    } man(--o);
+    out of the oo of the "min";
+    office o;
+}
